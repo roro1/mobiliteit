40a29446ac9ea0f2bd3b3c063f5e05f3a720ab8e
Ronald Haverman (PZH) <73708678+Ro-PZH@users.noreply.github.com>
2021-11-15 14:12:14 +0100

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -1,6 +1,6 @@
       var mapData = [
         {"lat": 52.15, "lon": 4.6  , "popup":"testpopup"},
-        {"lat": 52.17, "lon":4.7 , "popup":"pop333"}
+        {"lat": 52.17, "lon":4.7 , "popup":"pop333"},
         {"lat": 52.19, "lon":4.8 , "popup":"pop555"}
 
       ];
