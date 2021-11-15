ca4d3555a648543c0c8f69fc0a4541e54ce8f729
RR <ronald@haverman.com>
2021-11-15 15:17:00 +0100

    update

--- mapdata.js
+++ mapdata.js
@@ -1,5 +1,4 @@
       var mapData = [
-        {"lat": 52.15, "lon": 4.6  , "popup":"testpop"},
-        {"lat": 52.17, "lon":4.7 , "popup":"pop3"},
-        {"lat": 52.19, "lon":4.8 , "popup":"pop5"}
+        {"lat": 52.15, "lon": 4.6 ,"type" : "punt" , "popup":"test marker"},
+        {"lat": 52.075623, "lon":4.658615 , "type" : "cirkel" , "popup":"Beter Bereikbaar Gouwe <br> <a href='https://www.beterbereikbaargouwe.nl/' ' target='_blank' > projectsite </a>"}
       ];
