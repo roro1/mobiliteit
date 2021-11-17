f6794412dfd8e7dcfe14c23f0d391bafdfd64e00
RR <ronald@haverman.com>
2021-11-17 14:17:36 +0100

    update kaart..

--- mapdata.js
+++ mapdata.js
@@ -1,27 +1,40 @@
-      var mapData = [
-        {"titel":"Beter Bereikbaar Gouwe"  ,
-         "link":" <a href='https://www.beterbereikbaargouwe.nl/' ' target='_blank' > projectsite </a>",
-         "type" : "cirkel" , "latlon"  : [52.075623, 4.658615] ,  "straal": 3500 },
+var mapData = [
+  {"id":"1",
+   "titel":"Beter Bereikbaar Gouwe"  ,
+   "link" :"<a href='https://www.beterbereikbaargouwe.nl/' ' target='_blank' > projectsite </a>",
+   "type" :"cirkel","latlon":[52.075623, 4.658615],"straal": 3500,
+   "kort": "Gemeenten Alphen aan den Rijn, Waddinxveen en Bodegraven - Reeuwijk, de Regio Midden-Holland en de provincie Zuid-Holland werken samen in Beter Bereikbaar Gouwe. Het doel van dit programma is het verbeteren van de bereikbaarheid en leefbaarheid in het gebied rond de Gouwe en het versterken van de economie."},
 
-        {"titel":"MIRT onderzoek A12 Gouwe Oudenrijn / Bodegravenboog" ,
-         "link":"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
-         "type" : "polygoon", "poly": [ [52.078662, 4.737378],[52.062201, 4.733772],[52.066211, 4.751458] ] },
+  {"id":"2",
+   "titel":"MIRT onderzoek A12 Gouwe Oudenrijn / Bodegravenboog" ,
+   "link" :"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
+   "type" :"polygoon","poly":[[52.078662,4.737378],[52.062201,4.733772],[52.066211,4.751458]] },
 
-        {"titel":"MIRT planuitwerkingA20 Nieuwerkerk-Gouda" ,
-         "link":"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
-         "type" : "polygoon", "poly": [ [52.031859, 4.681618],[52.029789, 4.684811],[52.020283, 4.655972], [52.00286, 4.655285],[51.978748, 4.620438],[51.981285, 4.617004],[52.002109, 4.650667],[52.021762, 4.649963] ]},
+  {"id":"3",
+   "titel":"MIRT planuitwerkingA20 Nieuwerkerk-Gouda" ,
+   "link" :"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
+   "type" :"polygoon","poly":[[52.031859,4.681618],[52.029789,4.684811],[52.020283,4.655972], [52.00286,4.655285],[51.978748,4.620438],[51.981285, 4.617004],[52.002109, 4.650667],[52.021762, 4.649963] ]},
 
-         {"titel":"N207 zuid / Vredenburghlaan / Bentwoudlaan" ,
-          "link":"<a href='https://www.zuid-holland.nl/onderwerpen/verkeer-vervoer/wegverkeer/alle-n-wegen/wegen/n207/n207-zuid/' target='_blank' > projectsite </a>",
-          "type" : "polygoon", "poly": [ [52.099209, 4.584457],[52.100896, 4.593727],[52.024487, 4.642136],[52.024909, 4.633896] ] },
+  {"id":"4",
+   "titel":"N207 zuid / Vredenburghlaan / Bentwoudlaan" ,
+   "link" :"<a href='https://www.zuid-holland.nl/onderwerpen/verkeer-vervoer/wegverkeer/alle-n-wegen/wegen/n207/n207-zuid/' target='_blank' > projectsite </a>",
+   "type" :"polygoon", "poly": [ [52.099209, 4.584457],[52.100896, 4.593727],[52.024487, 4.642136],[52.024909, 4.633896] ] ,
+   "kort" :"De regio tussen Alphen en Waddinxveen kent knelpunten op gebied van bereikbaarheid en leefbaarheid, waarbij de bereikbaarheid bestaat uit doorstroming (N207 en N209) en veiligheid (in de kernen en van aansluitingen op de provinciale wegen). N207 Zuid is een eerste stap in het aanpakken van deze knelpunten. nieuwe wegen zoals de Bentwoudlaan en Verlengde Bentwoudlaan, nieuwe fiets- en wandelpaden en een ecologische verbindingszone worden aangelegd. Daarnaast wordt de structuur en aansluitingen op de N209 in Hazerswoude-Dorp aangepakt ."},
 
-         {"titel":"Zuidplaspolder / het vijfde dorp" ,
-          "link":"<a href='https://www.zuidplas.nl/de-nieuwe-zuidplaspolder' target='_blank' > Gemeente Zuidplas </a><br> <a href='https://www.omroepwest.nl/nieuws/4376720/waarom-wil-zuidplas-een-heel-nieuw-dorp-bouwen-tien-vragen-over-het-vijfde-dorp' target='_blank' > Vijfde dorp omroep west</a>",
-          "type" : "polygoon", "poly": [ [51.981666, 4.617691],[51.999425, 4.593487],[52.011683, 4.611511], [52.018909, 4.613228],[51.999784, 4.647732] ]},
+  {"id":"5",
+   "titel":"Zuidplaspolder / het vijfde dorp" ,
+   "link" :"<a href='https://www.zuidplas.nl/de-nieuwe-zuidplaspolder' target='_blank' > Gemeente Zuidplas </a><br> <a href='https://www.omroepwest.nl/nieuws/4376720/waarom-wil-zuidplas-een-heel-nieuw-dorp-bouwen-tien-vragen-over-het-vijfde-dorp' target='_blank' > Vijfde dorp omroep west</a>",
+   "type" :"polygoon", "poly": [ [51.981666, 4.617691],[51.999425, 4.593487],[52.011683, 4.611511], [52.018909, 4.613228],[51.999784, 4.647732] ]},
 
-         {"titel":"Veroers circulatie plan Gouda + Algera corridor" ,
-          "link":"test marker",
-          "type" : "punt" , "latlon":[ 52.01574, 4.709197]},
+  {"id":"6",
+   "titel":"Veroers circulatie plan Gouda + Algera corridor" ,
+   "link" :"nog geen links",
+   "type" :"punt" , "latlon":[ 52.01574, 4.709197]},
+
+   {"id":"7",
+    "titel":"Dijkversterking Krimpenerwaard" ,
+    "link" :"<a href='https://www.schielandendekrimpenerwaard.nl/actueel/krachtige-ijsseldijken-krimpenerwaard/' target='_blank' > projectsite Hoogheemraadschap</a>",
+    "type" :"polygoon", "poly": [ [51.917591, 4.58844],[51.997776, 4.69384],[51.99989, 4.684982], [51.923732, 4.577522] ]},
 
 
-          ];
+];
