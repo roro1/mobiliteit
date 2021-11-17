b9ff39d40bbd5260259e67d580a22e03c1baf70c
RR <ronald@haverman.com>
2021-11-17 11:40:29 +0100

    update kaart

--- mapdata.js
+++ mapdata.js
@@ -1,4 +1,27 @@
       var mapData = [
-        {"lat": 52.15, "lon": 4.6 ,"type" : "punt" , "popup":"test marker"},
-        {"lat": 52.075623, "lon":4.658615 , "type" : "cirkel" , "popup":"Beter Bereikbaar Gouwe <br> <a href='https://www.beterbereikbaargouwe.nl/' ' target='_blank' > projectsite </a>"}
-      ];
+        {"titel":"Beter Bereikbaar Gouwe"  ,
+         "link":" <a href='https://www.beterbereikbaargouwe.nl/' ' target='_blank' > projectsite </a>",
+         "type" : "cirkel" , "latlon"  : [52.075623, 4.658615] ,  "straal": 3500 },
+
+        {"titel":"MIRT onderzoek A12 Gouwe Oudenrijn / Bodegravenboog" ,
+         "link":"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
+         "type" : "polygoon", "poly": [ [52.078662, 4.737378],[52.062201, 4.733772],[52.066211, 4.751458] ] },
+
+        {"titel":"MIRT planuitwerkingA20 Nieuwerkerk-Gouda" ,
+         "link":"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
+         "type" : "polygoon", "poly": [ [52.031859, 4.681618],[52.029789, 4.684811],[52.020283, 4.655972], [52.00286, 4.655285],[51.978748, 4.620438],[51.981285, 4.617004],[52.002109, 4.650667],[52.021762, 4.649963] ]},
+
+         {"titel":"N207 zuid / Vredenburghlaan / Bentwoudlaan" ,
+          "link":"<a href='https://www.zuid-holland.nl/onderwerpen/verkeer-vervoer/wegverkeer/alle-n-wegen/wegen/n207/n207-zuid/' target='_blank' > projectsite </a>",
+          "type" : "polygoon", "poly": [ [52.099209, 4.584457],[52.100896, 4.593727],[52.024487, 4.642136],[52.024909, 4.633896] ] },
+
+         {"titel":"Zuidplaspolder / het vijfde dorp" ,
+          "link":"<a href='https://www.zuidplas.nl/de-nieuwe-zuidplaspolder' target='_blank' > Gemeente Zuidplas </a><br> <a href='https://www.omroepwest.nl/nieuws/4376720/waarom-wil-zuidplas-een-heel-nieuw-dorp-bouwen-tien-vragen-over-het-vijfde-dorp' target='_blank' > Vijfde dorp omroep west</a>",
+          "type" : "polygoon", "poly": [ [51.981666, 4.617691],[51.999425, 4.593487],[52.011683, 4.611511], [52.018909, 4.613228],[51.999784, 4.647732] ]},
+
+         {"titel":"Veroers circulatie plan Gouda + Algera corridor" ,
+          "link":"test marker",
+          "type" : "punt" , "latlon":[ 52.01574, 4.709197]},
+
+
+          ];
--- scr/app.js
+++ scr/app.js
@@ -1,17 +1,6 @@
-const taskList = document.querySelector('#task-list');
-const form = document.querySelector('#add-task-form');
-const modalForm = document.querySelector('#modal-form');
-const modeldeel1 = document.getElementById("modaldeel1");
+const PLijst = document.querySelector('#lijst');
 
-var xtasks =  []
-var inst = {
-  tekstlen: 40,
-  mode: "active", // hidded
-  extra: "Doe"
-};
 
-var liInModal = 0;
-//hideAddressBar();  //for mobile browsers
 
 function menuFunction() {
   var x = document.getElementById("myTopnav");
@@ -20,4 +9,49 @@ function menuFunction() {
   } else {
     x.className = "topnav";
   }
+}
+
+
+function plotDots(json) {
+    for(punti in json) {
+      punt = json[punti];
+      console.log(punti);
+
+      if (punt["type"] == "cirkel") {
+        console.log("een cirkel!");
+        marker =  L.circle(punt["latlon"], punt["straal"], {color: '#281f6b',fillColor: '#efcc36',fillOpacity: 0.1}).addTo(map);
+        }
+      else {
+         if (punt["type"] == "polygoon") {
+           console.log("een polygoon");
+           marker = L.polygon(punt["poly"],{color: '#281f6b',fillColor: '#efcc36',fillOpacity: 0.1}).addTo(map);
+         }
+       else {
+        console.log("een punt");
+      marker = L.marker(punt["latlon"]).addTo(map);
+      }
+    }
+      urltext= "<a href='https://eur03.safelinks.protection.outlook.com/ap/w-59584e83/?url=https%3A%2F%2Fpzh-my.sharepoint.com%2F%3Aw%3A%2Fg%2Fpersonal%2Fra_haverman_pzh_nl%2FEYVr7u137k1InYvJLCtjT8EBTb20vDB10Et3UOvOosGtUQ%3Fe%3DbWDWDT&data=04%7C01%7Cra.haverman%40pzh.nl%7C26720f2d8b224028bbbb08d9a819bf64%7C6d99bc288f284a73a50163a8e1eb3040%7C0%7C0%7C637725650795165583%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000&sdata=ZSMBJg%2FiawroEarPmYMVat2im%2FXkQxklmRBVMObxick%3D&reserved=0' target='_blank' > meer info (pzh only) </a>"
+      text = "<p><b>" + punt["titel"] +  "</b></p><p>" + punt["link"]+ "</p><p>" + urltext + "</p>";
+      marker.bindPopup(text);
+
+    }
+}
+
+function renderLijst(json){  // vul lijst met projecten op basis Json
+  for(punti in json) {
+    punt = json[punti];
+    console.log(punti);
+    console.log(punt);
+
+    let li = document.createElement('li');
+    PLijst.appendChild(li);
+    let task = document.createElement('span');
+    task.classList.add("tekst");
+    li.appendChild(task);
+    str = "<h3>" + punt["titel"] +"</h3><br>" + punt["link"]
+    task.innerHTML = str;
+//    task.addEventListener('click', (e) => {klikknop(e);  }); //voor klik op de tekst
+
+}
 }
