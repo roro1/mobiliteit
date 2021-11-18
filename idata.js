f08a35664f12c7ee932d309fdf4d1ea80123a83a
RR <ronald@haverman.com>
2021-11-18 11:57:24 +0100

    update

---
+++ idata.js
@@ -0,0 +1,14 @@
+var inspiratieData = [
+  {"id":"1",
+   "titel":"Brede welvaart en Mobiliteit"  ,
+   "link" :"<a href='https://www.pbl.nl/publicaties/brede-welvaart-en-mobiliteit' ' target='_blank' > brede-welvaart-en-mobiliteit </a>",
+   "kort": "In deze notitie verkent het PBL wat het betekent als mobiliteit en mobiliteitsbeleid vanuit een bredewelvaartsperspectief worden beschouwd. Hieruit destilleren we zowel implicaties voor het mobiliteitsbeleid als de nieuwe kennisbehoefte die daaruit voortkomt."},
+
+  {"id":"2",
+   "titel":"Nota Deeltweewielers Rotterdam" ,
+   "link" :"<a href='https://rotterdam.raadsinformatie.nl/document/10586393/1/s21bb010999_2_40029_tds' target='_blank' > Nota Deeltweewielers </a>",
+   "kort" :"Rotterdam streeft ernaar de mobiliteitstransitie verder te versnellen en overlast te voorkomen. In de nieuwe Nota Deeltweewielers geeft Rotterdam meer ruimte voor deeltweewielers en stuurt op kwaliteit." },
+
+
+
+];
--- scr/app.js
+++ scr/app.js
@@ -1,4 +1,4 @@
-const PLijst = document.querySelector('#lijst');
+const PContent = document.querySelector('#content');
 
 
 
@@ -27,30 +27,43 @@ function plotDots(json) {
       marker = L.marker(punt["latlon"]).addTo(map);
       }
     }
-      urltext= "<a href='https://eur03.safelinks.protection.outlook.com/ap/w-59584e83/?url=https%3A%2F%2Fpzh-my.sharepoint.com%2F%3Aw%3A%2Fg%2Fpersonal%2Fra_haverman_pzh_nl%2FEYVr7u137k1InYvJLCtjT8EBTb20vDB10Et3UOvOosGtUQ%3Fe%3DbWDWDT&data=04%7C01%7Cra.haverman%40pzh.nl%7C26720f2d8b224028bbbb08d9a819bf64%7C6d99bc288f284a73a50163a8e1eb3040%7C0%7C0%7C637725650795165583%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000&sdata=ZSMBJg%2FiawroEarPmYMVat2im%2FXkQxklmRBVMObxick%3D&reserved=0' target='_blank' > meer info (pzh only) </a>"
-      if (punt["id"] !=null ) {
+            if (punt["id"] !=null ) {
       text = "<a href='plannen.html?id="+punt["id"] + " ' > <b>" + punt["titel"] +  "</b></a> "
     } else {text = "."}
-      text = text + "<p>" + punt["link"]+ "</p><p>" + urltext + "</p>";
+      text = text + "<p>" + punt["link"]+ "</p>";
       marker.bindPopup(text);
 
     }
 }
 
-function renderLijst(json,wat){  // vul lijst met projecten op basis Json
-  for(punti in json) {
-    punt = json[punti];
-    if (wat == null || wat == punt["id"]) {
-    let li = document.createElement('li');
-    PLijst.appendChild(li);
-    let task = document.createElement('span');
-    task.classList.add("tekst");
-    li.appendChild(task);
-    str = "<h3>" + punt["titel"] +"</h3>"
-    if (punt["kort"]  != null) {str = str + "<p>" + punt["kort"] +"</p>"}
-    str = str + punt["link"]
-    task.innerHTML = str;
-//    task.addEventListener('click', (e) => {klikknop(e);  }); //voor klik op de tekst
+function renderLijst(json,wat,optie){  // vul lijst met items op basis Json
+  let xli = document.createElement('span');
+  if (optie == "inspiratie") {
+  xli.innerHTML = "<H1>Inspiratie</H1><ul id='lijst'></ul>";
+} else {
+  xli.innerHTML = "<H1>Plannen in Midden Holland</H1><ul id='lijst'></ul>";
+
 }
+  PContent.appendChild(xli); //append child in content blok
+  const PLijst = document.querySelector('#lijst');
+
+
+  for(itemi in json) {
+    punt = json[itemi];
+    if (wat == null || wat == punt["id"]) {
+      let li = document.createElement('li');
+      PLijst.appendChild(li);
+      let task = document.createElement('span');
+      task.classList.add("tekst");
+      li.appendChild(task);
+      str = "<h3>" + punt["titel"] +"</h3>"
+      if (punt["kort"]  != null) {str = str + "<p>" + punt["kort"] +"</p>"}
+      str = str + punt["link"]
+      if (wat == punt["id"]) {
+        urltext= "<a href='https://eur03.safelinks.protection.outlook.com/ap/w-59584e83/?url=https%3A%2F%2Fpzh-my.sharepoint.com%2F%3Aw%3A%2Fg%2Fpersonal%2Fra_haverman_pzh_nl%2FEYVr7u137k1InYvJLCtjT8EBTb20vDB10Et3UOvOosGtUQ%3Fe%3DbWDWDT&data=04%7C01%7Cra.haverman%40pzh.nl%7C26720f2d8b224028bbbb08d9a819bf64%7C6d99bc288f284a73a50163a8e1eb3040%7C0%7C0%7C637725650795165583%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000&sdata=ZSMBJg%2FiawroEarPmYMVat2im%2FXkQxklmRBVMObxick%3D&reserved=0' target='_blank' > meer info (pzh only) </a>"
+        str = str + "<br>" + urltext;
+      }
+      task.innerHTML = str;
+    }
 }
 }
