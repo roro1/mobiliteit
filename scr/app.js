69250fac4edee2f12a4e9d6c6bdd053c384e425e
RR <ronald@haverman.com>
2021-11-19 20:56:31 +0100

    update..

--- scr/app.js
+++ scr/app.js
@@ -16,14 +16,12 @@ function plotDots(json) {
     for(punti in json) {
       punt = json[punti];
 
-      if (punt["type"] == "cirkel") {
+      if (punt["geo"] == "cirkel") {
         marker =  L.circle(punt["latlon"], punt["straal"], {color: '#281f6b',fillColor: '#efcc36',fillOpacity: 0.1}).addTo(map);
-        }
-      else {
-         if (punt["type"] == "polygoon") {
+        } else {
+         if (punt["geo"] == "polygoon") {
            marker = L.polygon(punt["poly"],{color: '#281f6b',fillColor: '#efcc36',fillOpacity: 0.1}).addTo(map);
-         }
-       else {
+         } else {
       marker = L.marker(punt["latlon"]).addTo(map);
       }
     }
@@ -63,11 +61,16 @@ function renderLijst(json,wat,optie){  // vul lijst met items op basis Json
       li.appendChild(task);
       str = "<h3>" + punt["titel"] +"</h3>"
       if (punt["kort"]  != null) {str = str + "<p>" + punt["kort"] +"</p>"}
-      str = str + punt["link"]
+      if (punt["link"]  != null) {str = str + "<p><b>Links:</b> " + punt["link"] +"</p>"}
       if (wat == punt["id"]) {
-        urltext= "<a href='https://eur03.safelinks.protection.outlook.com/ap/w-59584e83/?url=https%3A%2F%2Fpzh-my.sharepoint.com%2F%3Aw%3A%2Fg%2Fpersonal%2Fra_haverman_pzh_nl%2FEYVr7u137k1InYvJLCtjT8EBTb20vDB10Et3UOvOosGtUQ%3Fe%3DbWDWDT&data=04%7C01%7Cra.haverman%40pzh.nl%7C26720f2d8b224028bbbb08d9a819bf64%7C6d99bc288f284a73a50163a8e1eb3040%7C0%7C0%7C637725650795165583%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000&sdata=ZSMBJg%2FiawroEarPmYMVat2im%2FXkQxklmRBVMObxick%3D&reserved=0' target='_blank' > meer info (pzh only) </a>"
-        str = str + "<br>" + urltext;
-      }
+        if (punt["rel"]  != null) {str = str + "<p><b>Gerelateerd aan:</b> " + punt["rel"] +"</p>"}
+        if (punt["pzhlink"]  != null) {str = str + "<p><b>PZH intranet:</b> " + punt["pzhlink"] +"</p>"}
+        if (punt["idmslink"]  != null) {str = str + "<p><b>PZH IDMS links:</b> " + punt["idmslink"] +"</p>"}
+        if (punt["lang"]  != null) {str = str + "<p>" + punt["lang"] +"</p>"}
+
+      } else {       if (punt["id"] !=null ) {
+      str = str + "<a href='plannen.html?id="+punt["id"] + "'> <b><i>Meer...</i></b></a> "
+    }}
       task.innerHTML = str;
     }
 }
