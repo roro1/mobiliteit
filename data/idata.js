05e0ea1c64b50238e145b517df78885cd9b6cf0a
Ronald Haverman (PZH) <73708678+Ro-PZH@users.noreply.github.com>
2021-11-22 09:18:41 +0100

    Update idata.js
    update insp

--- data/idata.js
+++ data/idata.js
@@ -1,4 +1,13 @@
 var inspiratieData = [
+  {"id":"3",
+   "titel":"Minder vliegen, minder met de auto, meer met de trein in de Eurodelta"  ,
+   "link" :"<a href='https://www.zuid-holland.nl/actueel/nieuws/november-2021/minder-vliegen-minder-auto-trein-eurodelta/' target='_blank' > Minder vliegen, minder met de auto, meer met de trein in de Eurodelta </a>",
+   "kort": "Mensen in de Eurodelta moeten gestimuleerd worden om meer met de trein te reizen en juist minder met het vliegtuig en de auto. Dat stellen onderzoekers van ESPON in opdracht van overheden die nauw samenwerken in de Eurodelta: het gebied dat ligt op Nederlands, Belgisch, Frans en Duits grondgebied, grofweg in de driehoek Amsterdam –Lille - Keulen. bekijk het filmpje op voor een impressie van de conclusies. Mobility as a service, Hubs en deelmobiliteit worden ook aanbevolen"},
+
+
+
+
+
   {"id":"2",
    "titel":"Utrecht gaat voor ov-impuls vanaf 2025"  ,
    "link" :"<a href='https://www.ovmagazine.nl/nieuws/utrecht-gaat-voor-ov-impuls-vanaf-2025' target='_blank' > brede-welvaart-en-mobiliteit </a>",
