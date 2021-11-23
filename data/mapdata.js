d502ea072fca3e91eb18c995bdf6981b993ab31e
Ronald Haverman (PZH) <73708678+Ro-PZH@users.noreply.github.com>
2021-11-23 09:43:28 +0100

    update data

--- data/mapdata.js
+++ data/mapdata.js
@@ -3,7 +3,7 @@ var mapData = [
    "titel":"Regio Midden Holland"  ,
    "link" :"<a href='https://www.regiomiddenholland.nl/default.aspx' ' target='_blank' > Regio Midden-Holland </a>; <a href='https://youtu.be/NXhC46ucpp0' ' target='_blank' > Filmpje Kracht van Midden-Holland </a> ",
    "geo" :"punt","latlon":[52.075623, 4.658615],"straal": 3500,
-   "kort": "Regio Midden-Holland is een netwerk van de samenwerkende gemeenten <a href='https://www.bodegraven-reeuwijk.nl' target='_blank' >Bodegraven-Reeuwijk</a>; <a href='https://www.gouda.nl'  target='_blank'>Gouda</a>; <a href='https://www.krimpenerwaard.nl'  target='_blank'>Krimpenerwaard</a>; <a href='https://www.waddinxveen.nl'  target='_blank'>Waddinxveen</a>; <a href='http://www.zuidplas.nl' target='_blank'>Zuidplas</a>."
+   "kort": "<p>Regio Midden-Holland is een netwerk van de samenwerkende gemeenten <a href='https://www.bodegraven-reeuwijk.nl' target='_blank' >Bodegraven-Reeuwijk</a>; <a href='https://www.gouda.nl'  target='_blank'>Gouda</a>; <a href='https://www.krimpenerwaard.nl'  target='_blank'>Krimpenerwaard</a>; <a href='https://www.waddinxveen.nl'  target='_blank'>Waddinxveen</a>; <a href='http://www.zuidplas.nl' target='_blank'>Zuidplas</a>. In 2016 is een strategische agenda voor de regio Midden-Holland vastgesteld. Hieruit wordt nog volop gewerkt en er is een nieuwe versie verschenenen (voor periode vanaf 2020) onder de noemer ‘De kracht van Midden-Holland: de regio van verbinding’</p> <p>Hieronder staan een aantal plannen, projecten en programma's  die in deze regio spelen.</p>"
   },
 
 
@@ -23,6 +23,7 @@ var mapData = [
   {"id":"3",
    "titel":"MIRT planuitwerkingA20 Nieuwerkerk-Gouda" ,
    "link" :"<a href='https://mirta20nieuwerkerkgouda.nl/over+het+project1/default.aspx' target='_blank' > projectsite </a>",
+   "kort": "Het deel van de A20 tussen Nieuwerkerk aan den IJssel en Gouda is een grote bottleneck voor het verkeer. Dit weggedeelte is het enige deel van de A20 dat uit 2x2 rijstroken bestaat. De rest van het tracé heeft 2x3 rijstroken. In beide richtingen staan dagelijks files en er gebeuren veel ongevallen. Het projectgebied is het traject van de A20 vanaf de aansluiting Nieuwerkerk aan den IJssel (km 41,5) tot aan de A12-afslag Gouda (km 29,3). Dit traject heeft een lengte van circa 10 kilometer. De aansluitingen met onderliggende infrastructuur en van de A20 op de A12 vallen binnen het project, inclusief het Gouwe-aquaduct.",
    "geo" :"polygoon","poly":[[52.031859,4.681618],[52.029789,4.684811],[52.020283,4.655972], [52.00286,4.655285],[51.978748,4.620438],[51.981285, 4.617004],[52.002109, 4.650667],[52.021762, 4.649963] ]},
 
   {"id":"4",
@@ -35,16 +36,18 @@ var mapData = [
 
   {"id":"5",
    "titel":"Zuidplaspolder / het vijfde dorp" ,
+   "kort" : "Er wordt al sinds 2004 over een dorp in de Zuidplaspolder gesproken. De gemeente Zuidplas een masterplan gepresenteerd. De gemeente Zuidplas bestaat uit vier kernen, Nieuwerkerk aan den IJssel, Zevenhuizen, Moerkapelle en Moordrecht. Het nieuwe dorp komt in het Middengebied van de Zuidplaspolder te liggen. Tussen Nieuwerkerk aan den IJssel, Gouda en Zevenhuizen in ligt nu nog een onbebouwd stuk grond.",
    "link" :"<a href='https://www.zuidplas.nl/de-nieuwe-zuidplaspolder' target='_blank' > Gemeente Zuidplas </a><br> <a href='https://www.omroepwest.nl/nieuws/4376720/waarom-wil-zuidplas-een-heel-nieuw-dorp-bouwen-tien-vragen-over-het-vijfde-dorp' target='_blank' > Vijfde dorp omroep west</a>",
    "geo" :"polygoon", "poly": [ [51.981666, 4.617691],[51.999425, 4.593487],[52.011683, 4.611511], [52.018909, 4.613228],[51.999784, 4.647732] ]},
 
   {"id":"6",
-   "titel":"Veroers circulatie plan Gouda + Algera corridor" ,
+   "titel":"Veroerscirculatieplan Gouda" ,
    "link" :"nog geen links",
    "geo" :"punt" , "latlon":[ 52.01574, 4.709197]},
 
    {"id":"7",
     "titel":"Dijkversterking Krimpenerwaard" ,
+    "kort" : "De dijk langs de Hollandsche IJssel beschermt de Krimpenerwaard. Om ook in de toekomst het gebied te kunnen beschermen moet de dijk versterkt en verhoogd worden.",
     "link" :"<a href='https://www.schielandendekrimpenerwaard.nl/actueel/krachtige-ijsseldijken-krimpenerwaard/' target='_blank' > projectsite Hoogheemraadschap</a>",
     "geo" :"polygoon", "poly": [ [51.917591, 4.58844],[51.997776, 4.69384],[51.99989, 4.684982], [51.923732, 4.577522] ]},
 
