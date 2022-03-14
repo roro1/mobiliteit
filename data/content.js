6a4bda73922c001055ed6f6332d4e0d931c8e29d
Ronald Haverman (PZH) <73708678+Ro-PZH@users.noreply.github.com>
2022-03-14 20:42:25 +0100

    rapport2

--- data/content.js
+++ data/content.js
@@ -1,9 +1,10 @@
 var contData = [
 
-"$6<h1>Zuid-Holland in transitie</h1><p>Er komen grote veranderingen op ons af. Oók in Zuid-Holland. In deze online publicatie/Snelstudie {we zoeken nog hipper woord} beschrijven we de uitdagingen en problemen die we zien. We komen ook met elementen die bij zouden kunnen dragen aan een oplossingen. We kunnen niet in een glazen bol kijken. Het gaat niet om vaststaand beleid van de provincie Zuid-Holland, maar om denk-lijnen, inspiratie en ideeën om samen op verder te borduren. En waar we graag met iedereen in Zuid-Holland over in gesprek gaan. Op weg naar een Zuid-Holland dat goed bereikbaar is en fijn is om in te leven. Nu en in de toekomst.</p><details><summary>Contact met de provincie Zuid-Holland?</summary>......</details>"  ,
+"$6<h1>Zuid-Holland in transitie - Uitdagingen</h1><p>Er komen grote veranderingen op ons af. Oók in Zuid-Holland. In deze online publicatie/Snelstudie {we zoeken nog hipper woord} beschrijven we de uitdagingen en problemen die we zien. We komen ook met elementen die bij zouden kunnen dragen aan een oplossingen. We kunnen niet in een glazen bol kijken. Het gaat niet om vaststaand beleid van de provincie Zuid-Holland, maar om denk-lijnen, inspiratie en ideeën om samen op verder te borduren. En waar we graag met iedereen in Zuid-Holland over in gesprek gaan. Op weg naar een Zuid-Holland dat goed bereikbaar is en fijn is om in te leven. Nu en in de toekomst.</p><details><summary>Contact met de provincie Zuid-Holland?</summary>......</details>"  ,
 
 
-"$3<h2>Inhoud</h2><ol><li>Aanleiding en problemen</li><li>Oplossingsrichtingen</li><li>Drie (voorbeeld)situaties</li><li>Conclusies en aanbevelingen</li></ol>",
+"$3<h2>Inhoud</h2><ol><li>$[Uitdagingen](rapport.html)</li><li>$[Kansen & oplossingsrichtingen](rapport2.html)</li><li>Drie (voorbeeld)situaties</li><li>Conclusies en aanbevelingen</li></ol>",
+
 "$a<img src='https://www.europoortkringen.nl/wp-content/uploads/2014/01/Shell-Pernis.png' width='100%' height='auto'>",
 "$6<h2>Afhankelijkheid van fossiele brandstoffen</h2> <p>Om met het meest actuele reden voor een transitie te beginnen: De enorme afhankelijkheid van fossiele brandstoffen (olie, benzine, gas) maakt ons kwetsbaar voor de grillen van buitenlandse regiems. {{verwijzen LINKS} afhankelijkheid Russisch gas en olie.</p>",
 "$a<img src='https://images0.persgroep.net/rcs/ROk4o885KNFBqCyJcWzTMzic3Ag/diocontent/66626232/_fill/1600/876/?appId=21791a8992982cd8da851550a453bd7f&quality=0.9' width='100%' height='auto'>",
