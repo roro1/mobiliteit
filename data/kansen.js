2e1dcffa9ebd32010367915cf3fbb7e9d160e53c
Ronald Haverman (PZH) <73708678+Ro-PZH@users.noreply.github.com>
2022-11-04 13:55:16 +0100

    kansenKaart

---
+++ data/kansen.js
@@ -0,0 +1,141 @@
+var kansen = [
+
+  {"id":"64",
+   "titel":"N214/N216 Gelijkvloerse Kruising"  ,
+   "type" :"weg",
+   "regio":"Drecht",
+   "link" :"$[N214/N216 Gelijkvloerse Kruising](https://www.zuid-holland.nl/onderwerpen/verkeer-vervoer/wegverkeer/alle-n-wegen/wegen/n214/n214-n216/) ; ",
+   "kort": "Het aanpassen van de gelijkvloerse kruising N214/N216 is een van de maatregelen om de doorstroming in de regio te verbeteren. Met deze aanpassingen worden de huidige wachttijden in de spits verminderd.",
+   "geo": "punt" , "latlon": [51.881381,4.895564],
+  },
+
+  {"id":"66",
+   "titel":"Concordiabrug"  ,
+   "type" :"weg",
+   "regio":"Drecht",
+   "link" :"$[Concordiabrug](https://nl.wikipedia.org/wiki/Bestand:Concordiabrug_Gorinchem.jpg) ; ",
+   "kort": "...",
+   "geo": "punt" , "latlon": [51.836261,4.974052],
+  },
+
+  {"id":"44",
+   "titel":"Waterstoftankstation Heinenoord"  ,
+   "type" :"OV",
+   "regio":"GH",
+   "link" :"$[Nieuw waterstoftankstation voor bussen in Heinenoord](https://www.ovpro.nl/bus/2022/03/14/nieuw-waterstoftankstation-voor-connexxion-bussen-in-heinenoord/) ; ",
+   "kort": "Twintig bussen tanken voortaan bij het nieuwe waterstoftankstation in het Zuid-Hollandse Heinenoord. Dat kunnen er nog meer worden, want het tankstation heeft capaciteit om ruim vijftig bussen te voorzien van waterstof. ",
+    "geo": "punt" , "latlon":[51.815047, 4.49589],
+    "lang": "Het tankstation is tot stand gekomen door een samenwerking tussen de provincie Zuid-Holland, de gemeente Hoeksche Waard, Connexxion en Everfuel. De waterstofleverancier gaat minimaal twaalf jaar groene waterstof leveren aan het tankstation. In de toekomst kunnen ook vrachtwagens er gebruik van maken. Volgens Everfuel is Heinenoord één van de grootste waterstoftankstations in Europa.$![..](https://www.ovpro.nl/wp-content/uploads/2022/03/Opening-waterstoftankstation-Heinenoord-scaled.jpg)"
+  },
+
+  {"id":"46",
+   "titel":"Thuiswerkhub Middelharnis"  ,
+   "regio":"GH",
+   "geo": "punt" , "latlon":[51.762606, 4.189653],
+   "link" :"$[Thuiswerkhub](https://www.thuiswerkhub.nl/nieuws/) ; $[Thuiswerkhub bericht + rapport](https://www.zuid-holland.nl/actueel/nieuws/november-2021/dichtbij-huis-werken-thuiswerkhub/) ",
+   "kort": "De eerste Thuiswerkhub heeft zijn deuren geopend. Op een toplocatie in Middelharnis kun je nu thuiswerken in de buurt van je huis. Handelsgebouw De Oostplaat, zoals de thuiswerkhub heet, combineert de voordelen van werken op kantoor met het gemak van thuiswerken. Zie ook de Snelstudie Thuiswerkhubs.",
+   "lang" : "$![..](https://www.zuid-holland.nl/publish/pages/28510/546px/afbeelding-thuiswerkhub-bij-bericht-thuiswerkhub-in-middelharnis_1.jpg)"
+  },
+
+  {"id":"48",
+   "titel":"Pontje Oud Beijerland – Rhoon"  ,
+   "type" :"fiets",
+   "regio":"GH",
+   "link" :"$[Blue Amigo Veerdienst](https://www.blueamigo.com/veerdienstrhoon/) ; ",
+   "kort": "Pontje Oud Beijerland – Rhoon geeft verbinding per fiets naar metrostation Rhoon.",
+   "geo": "punt" , "latlon":[51.828961,4.412441]
+  },
+
+  {"id":"51",
+   "titel":"P&R Numansdorp en Heinenoord "  ,
+   "regio":"GH",
+   "type" : "OV",
+   "geo": "punt" , "latlon":[51.81534, 4.494009],
+   "link" :"$[PZH site PenR](https://www.zuid-holland.nl/actueel/nieuws/februari-2022/parkeer-reis-verder-stad-ov/) ",
+   "kort": "Zuid-Holland heeft in samenwerking met de gemeente Hoeksche Waard 2 P+R’s gerealiseerd: 1 in Heinenoord en 1 in Numansdorp. Op een P+R kunnen automobilisten hun auto parkeren en met het openbaar vervoer verder de stad in reizen. Hiermee dragen P+R’s bij aan de bereikbaarheid en leefbaarheid van steden.",
+   "lang" : "$![..](https://www.hoekschnieuws.nl/wp-content/uploads/2021/01/3032_1200_1200_overlay-scaled.jpg)"
+  },
+
+  {"id":"52",
+   "titel":"Veer Nieuw-Beijerland – Hekelingen frequentieverhogen"  ,
+   "type" :"fiets",
+   "regio":"GH",
+   "link" :"$[thequeenjacqueline.nl/](https://www.thequeenjacqueline.nl/) ; ",
+   "kort": "Veer Hekelingen – Nieuw-Beijerland vormt een cruciale verbinding voor vitale beroepen. Hogere frequentie voor met name tbv fietsverkeer naar metro Spijkenisse. ",
+   "lang" : "$![](https://www.thequeenjacqueline.nl/wpsite/wp-content/uploads/2011/12/escohoogehuis.jpg)",
+   "geo": "punt" , "latlon":[51.816251,4.338541]
+  },
+
+
+
+  {"id":"33",
+   "titel":"Bleizo west"  ,
+   "regio":"mrdh",
+   "link" :"$[Factsheet Interne PZH link](https://pzh-my.sharepoint.com/:w:/g/personal/ra_haverman_pzh_nl/ERiBzNumBaNMl635WrFPq7YBBuAesmBvFJJ9Sy1QKJ88YQ?e=VwC6Ld) ",
+   "kort": "Er is grote discussie over de invulling van de locatie Bleizo-West, in het verleden aangewezen als bedrijventerrein met opties voor windenergie. Dit terwijl er nu een gemeentelijke wens ligt om hier duizenden woningen te realiseren. Vanuit mobiliteit heeft dit grote gevolgen voor de N209 (en ook N471) en er ligt een directe relatie met het nieuwe station Lansingerland",
+   "lang" : "",
+   "geo": "punt" , "latlon":[52.0432,4.523004]
+  },
+
+    {"id":"37",
+     "titel":"N222 rotonde Zwethlaan"  ,
+     "type" :"weg",
+     "regio":"mrdh",
+     "link" :"$[..](..) ",
+     "kort": "De rotonde Zwethlaan is onderdeel van de zogenaamde veilingroute. Er ligt een wens om deze uit te breiden tot een turbo rotonde om de doorstroming te verbeteren. Inmiddels op dit dossier diverse ontwikkelingen die voortgang onzeker maken ",
+     "geo": "punt" , "latlon":[ 51.995642, 4.26355021]
+    },
+
+
+
+  {"id":"23",
+   "titel":"Stationsgebied Leiden",
+   "type" :"OV",
+   "regio":"rijnland",
+   "geo": "punt" , "latlon":[52.165965, 4.48263],
+   "link" :"$[Stationsgebied Leiden gemeente](https://gemeente.leiden.nl/inwoners-en-ondernemers/werkzaamheden-in-leiden/bouwen-aan-de-stad/stationsgebied/) ; $[Leiden Verbindt - startnotitie voor MIRT verkenning](https://leiden.notubiz.nl/document/9336244/1#search=%22leiden%20verbindt%22)",
+   "kort": "Knooppunt Leiden Centraal is volop in ontwikkeling. Het internationale, krachtige kenniscluster Leiden Bio Science Park (LBSP) ontwikkelt door tot Innovation District. Afspraken over stedelijke en regionale verstedelijking in de invloedssfeer van de OV-knoop zorgen voor verder groei van de reizigersstromen van OV, fiets en voetganger. De ontwikkeling van de historische binnenstad met kennis- en cultuurclusters en veel voorzieningen aan de ene zijde van het knooppunt en het dynamische LBSP aan de andere zijde van het knooppunt kunnen elkaar verder versterken."
+  },
+
+  {"id":"2B",
+   "titel":"Valkenhorst  HOV-halte + Woningbouw + Unmanned Valley",
+   "type" :"wonen",
+   "regio":"midden",
+   "link" :"$[Valkenhorst](https://www.zuid-holland.nl/actueel/nieuws/december-2021/ontwikkeling-valkenhorst-forse-stap-dichterbij/)",
+   "geo": "punt" , "latlon":[52.172809, 4.42071],
+   "kort": "Deelmobiliteit , Woningbouw (700 woningen) + Unmanned Valley"
+  },
+
+
+  {"id":"6",
+   "titel":"Veroerscirculatieplan Gouda" ,
+   "regio":"midden",
+   "link" :"$[Veroerscirculatieplan Gouda](https://www.gouda.nl/gouda-werkt-aan-de-stad/verkeerscirculatieplan/)",
+   "geo" :"punt" , "latlon":[ 52.01574, 4.709197]},
+
+
+
+      {"id":"10",
+       "titel":"Citybarge " ,
+       "kort" : "Citybarge hernieuwt de vergeten grachten. Steden hebben te maken met verdichting en daardoor veel verkeer en logistiek in de binnenstad. Hierdoor krijgen we steden met een slechte leefbaarheid, luchtkwaliteit en veel CO2 uitstoot. CityBarge draagt bij aan de leefbaarheid van steden door duurzame, betaalbare en makkelijke waterlogistieke oplossingen voor de binnenstad. Hierbij maken we gebruik van de bestaande waterinfrastructuur in veel steden: de grachten.",
+       "link" :"$[Website Citybarge](https://citybarge.eu/nl_nl/)",
+         "lang": "$&Citybarge](https://www.youtube.com/embed/0VV3-O6FM4w)",
+       "geo" :"punt", "latlon": [52.00498, 4.360837]},
+
+       {"id":"11",
+          "regio":"midden",
+        "titel":"Idee bushalte Nieuwerkerk aan de IJssel op N219 " ,
+        "kort" : "Wens Nieuwerkerk ad IJssel (niet per se Krimpenerwaard, maar ligt hier wel in de buurt) voor een bushalte langs de provinciale weg N219 ter hoogte van de rotonde met de ’s Gravenweg. Met de nieuwbouwwijk en afstand van 1,8 km tot het treinstation. Fysieke inpassingsruimte ontbreekt. Verbeteren van de fiets en loopbereikbaarheid van het station Nieuwerkerk is ook aantrekkelijk voor bewoners in de nieuwe wijk",
+        "geo" :"punt", "latlon": [51.959239280555124, 4.618806388376384]},
+
+         {"id":"13",
+          "titel":"Reconstructie Grote Kruising (N210)." ,
+       "regio":"midden",
+       "type" :"weg",
+          "kort" : "De Algeracorridor is de enige rechtstreekse route van Krimpen aan den IJssel richting Capelle aan de IJssel en Rotterdam. Dagelijks gaan er grote auto-, OV- en fietsstromen over de corridor richting Capelle, Rotterdam, en verder de metropoolregio in.  In de ochtend- en avondspits zorgt dit voor stremmingen op de Algerabrug en kruisingen. Een van de grootste knelpunten in de ochtendspits voor autoverkeer is de Grote Kruising (N210). Ook voor fiets en openbaar vervoer (bussen) liggen er kansen om de doorstroming te verbeteren. Aanleiding voor de aanpassingen is de verzakking van de kruising. Het project draait om het bevorderen van de doorstroming van het auto-, fiets- en OV verkeer. De provincie draagt vanuit het programma KTA-MOVE bij aan de realisatie van dit project.",
+          "link" : "$[degrotekruising](https://www.degrotekruising.nl/)",
+          "geo" :"punt", "latlon": [51.91175, 4.589597]},
+
+  
+
+];
